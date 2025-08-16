0026cfef5393a0938fe50cad6f47f41ac1fb59da
woo-rin <dlqk091028@gmail.com>
2025-08-16 23:22:03 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t, k, n, i, j, l;
+    int arr[15][15] = {0, };
+    
+    for(i = 0; i < 15; i++) arr[0][i] = i;
+    
+    for(i = 1; i < 15; i++)
+        for(j = 1; j < 15; j++)
+            arr[i][j] = arr[i-1][j] + arr[i][j-1];
+    
+    scanf("%d", &t);
+    
+    for(i = 0; i < t; i++)
+    {
+        scanf("%d %d", &k, &n);
+        printf("%d\n", arr[k][n]);
+    }
+    return 0;
+}
