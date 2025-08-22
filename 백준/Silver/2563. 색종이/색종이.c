55d58f0e16363ddd032ccb4ea8bf0b70c3b32bb0
woo-rin <dlqk091028@gmail.com>
2025-08-22 11:08:37 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.c
+++ 백준/Silver/2563. 색종이/색종이.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
+int main (){
+  int ddd[100][100] = {0};
+  int a, b, c;
+  int ans=0;
 
-int main() {
-    int arr[100][100] = {0};
-    int n, r, c;
-    int ans = 0;
-
-    scanf("%d", &n);
-
-    for(int k = 0; k < n; k++) {
-        scanf("%d %d", &r, &c);
-        for(int i = r; i < r + 10; i++) {
-            for(int j = c; j < c + 10; j++) {
-                arr[i][j] += 1;
-                if(arr[i][j] == 1) {
-                    ans += 1;
-                }
-            }
+  scanf("%d", &a);
+  for(int k=0; k<a; k++){
+    scanf("%d %d", &b, &c);
+    for(int i=b; i<b+10; i++){
+      for(int j=c; j<c+10; j++){
+        ddd[i][j] +=1;
+        if(ddd[i][j] == 1){
+          ans +=1;
         }
+      }
     }
-
-    printf("%d\n", ans);
-
-    return 0;
+  }
+  printf("%d\n", ans);
+  return 0;
 }
