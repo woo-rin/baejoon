ad8bc29f96b1b21ebe40f84271f69eba3843f85e
woo-rin <dlqk091028@gmail.com>
2025-08-22 10:27:17 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.c
+++ 백준/Silver/2563. 색종이/색종이.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
-const int RANGE = 10;
-int ans = 0;
-int arr[100][100] = {0,};
+int main() {
+    int arr[100][100] = {0};
+    int n, r, c;
+    int ans = 0;
 
-void increase(int r, int c) {
-    for (int i = r; i < r+RANGE; i++) {
-        for (int j = c; j < c+RANGE; j++) {
-            if (++arr[i][j] == 1)
-                ans++;
+    scanf("%d", &n);
+
+    for(int k = 0; k < n; k++) {
+        scanf("%d %d", &r, &c);
+        for(int i = r; i < r + 10; i++) {
+            for(int j = c; j < c + 10; j++) {
+                arr[i][j] += 1;
+                if(arr[i][j] == 1) {
+                    ans += 1;
+                }
+            }
         }
     }
-}
 
-int main() {
-    int arr[100][100] = {0,};
-    int n, r, c;
-    scanf("%d", &n);
-    while (n--) {
-        scanf("%d %d", &r, &c);
-        increase(r, c);
-    }
-    printf("%d", ans);
+    printf("%d\n", ans);
+
     return 0;
 }
