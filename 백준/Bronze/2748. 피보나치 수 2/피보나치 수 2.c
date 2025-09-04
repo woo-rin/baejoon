101a9133a1605c71ad9fba8b2c405a908534ef58
woo-rin <dlqk091028@gmail.com>
2025-09-05 08:22:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    long long arr[91] = {0, 1};
+
+    for (int i = 2; i <= n; i++)
+        arr[i] = arr[i - 1] + arr[i - 2];
+
+    printf("%lld", arr[n]);
+    return 0;
+}
