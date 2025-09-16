87038018f85228f2f35bc683cc390c7f3a96a8b1
woo-rin <dlqk091028@gmail.com>
2025-09-16 22:01:43 +0900

    [Bronze III] Title: 369, Time: 616 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,8 +1,5 @@
-n = int(input())
-
-result = 0 
-for num in range(1, n+1):
-    for i in str(num):
-        if i == '3' or i == '6' or i == '9':
-            result += 1
-print(result)
+N = int(input())
+cnt = 0
+for i in range(1, N+1):
+    cnt += str(i).count('3')+str(i).count('6')+str(i).count('9')
+print(cnt)
